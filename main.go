4ea16e3ce960054dceb4cae04ba6df5467dd33d4
Simon Sicard <sicsim64@gmail.com>
2024-02-10 19:30:14 +0100

    test

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
 }
 
 func main() {
@@ -28,6 +29,13 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{Name: "ashish", City: "New Delhi", Zipcode: "110075"},
 		{Name: "simon", City: "Paris", Zipcode: "75015"},
 	}
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+
 }
